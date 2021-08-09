b790d64f62cb0a502140421e9731019cf7dcff82
maglin07 <linmag138@outlook.com>
2021-08-09 12:59:42 -0400

    spring boot initizr

---
+++ src/test/java/com/example/springinitializr/myfirstspringbootapplication/MyFirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinitializr.myfirstspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
